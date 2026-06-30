aaba7099f7f1afd6f274cf489c1a0701881e9db9
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-06-30 14:57:05 +0530

    Add files via upload

---
+++ program73.c
@@ -0,0 +1,17 @@
+//INPUT: 5
+//OUTPUT: 1 2 3 4 5
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+    iCnt = 1;
+    while(iCnt<=5 )
+    {
+        printf("%d\t",iCnt);
+        iCnt++;
+    }
+
+    return 0;
+}
