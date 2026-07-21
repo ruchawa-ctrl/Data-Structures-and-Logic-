a7f49c729e93b48310ae11872616d4d9d8853b54
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-07-21 08:39:15 +0530

    Add files via upload

---
+++ program509.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Display()
+{
+    static int i = 0;
+
+    i = 1;  // Issue
+    if( i <= 4 )
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+    
+    return 0;
+}
