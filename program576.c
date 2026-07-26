79690b06358c48933daca0944a70db387f96d3ee
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-07-26 09:52:01 +0530

    Add files via upload

---
+++ program576.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>  // Only LINUX based os
+
+int main()
+{
+    int fd = 0;
+
+    fd = open("Marvellous.txt",O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File gete succesfully opened with fd : %d\n",fd);
+
+        write(fd,"Jay Ganesh...",13);
+
+        close(fd);
+    }
+
+    return 0;
+}
---
+++ program579.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>  // Only LINUX based os
+
+int main()
+{
+    int fd = 0;
+    int iRet = 0;
+    char Data[] = "Marvellous Infosystems";
+
+    fd = open("Marvellous.txt",O_RDWR | O_APPEND);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File gete succesfully opened with fd : %d\n",fd);
+
+        iRet = write(fd,Data,10);
+
+        printf("%d bytes gets succesfully written \n",iRet);
+        
+        close(fd);
+    }
+
+    return 0;
+}
