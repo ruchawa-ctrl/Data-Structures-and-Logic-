a1e029f1e0c3f3482f19b31ea1da7b54feda6d88
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-08-02 09:23:44 +0530

    Add files via upload

---
+++ program598.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+
+# define BUFFER_SIZE 1024
+
+void DisplayFile(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int fd = 0, iRet = 0;
+
+    fd = open(FileName,O_RDONLY);
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return;
+    }
+
+    while((iRet = read(fd,Buffer,sizeof(Buffer))) != 0)
+    {
+        write(1,Buffer,iRet);
+        memset(Buffer,'\0',sizeof(Buffer));
+    }
+
+    close(fd);
+}
+
+int main()
+{   
+    char Fname[30] = {'\0'};
+
+    printf("Enter the file name : \n");
+    scanf("%[^'\n']s",Fname);
+
+    DisplayFile(Fname);
+
+    return 0;
+}
