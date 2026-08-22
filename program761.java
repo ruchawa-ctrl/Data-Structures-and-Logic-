1a87698e92e83ab7acf746b14b2ec69fe45848a0
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-08-22 08:57:56 +0530

    Add files via upload

---
+++ program761.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class program761
+{
+    public static void main(String A[])
+    {
+        char Arr[] = {'d','c','a','b'};
+
+        Arrays.sort(Arr);
+
+        System.out.println(new String(Arr));
+    }
+}
