e08a131ce4147da29280a2c767a7b27de78af494
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-09-04 18:15:31 +0530

    Add files via upload

---
+++ program862.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class program862
+{
+    public static void main(String A[])
+    {
+        HashMap <Integer, String> hobj = new HashMap<Integer, String>();
+
+        hobj.put(20,"C Programming");
+        hobj.put(10,"Java Programming");
+        hobj.put(30,"C Programming");
+
+        System.out.println(hobj);
+    }
+}
---
+++ program872.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+interface GetterSetter
+{
+    void Accept();
+    void Display();
+}
+
+class ArrayX implements GetterSetter
+{
+    protected int Arr[];
+    protected int iSize;
+
+    public ArrayX(int iSize)
+    {
+        this.iSize = iSize;
+        Arr = new int[iSize];
+    }
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of array");
+
+        for(int i = 0; i < this.iSize; i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+    }
+
+    public void Display()
+    {
+        System.out.println("Elements of the array are : ");
+
+        for(int i = 0; i < this.iSize; i++)
+        {
+            System.out.print(Arr[i]+"\t");
+        }
+
+        System.out.println();
+    }
+}
+
+class Searching extends ArrayX
+{
+    public Searching(int iSize)
+    {
+        super(iSize);
+    }    
+}
+
+class program872
+{
+    public static void main(String A[])
+    {
+        Searching sobj = new Searching(5);
+        
+        sobj.Accept();
+
+        sobj.Display();
+    }
+}
