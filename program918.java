63804dc40cad460b5551e4014bbfdf53f453b53c
Rucha Walvekar <ruchawalvekar@gmail.com>
2026-09-10 08:14:34 +0530

    Add files via upload

---
+++ program918.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+class program918
+{
+    public static void main(String A[])
+    {
+        try
+        {
+            ServerSocket serversocket = new ServerSocket(9000);
+
+            System.out.println("-------------------------------------");
+            System.out.println("----- Marvellous Server Started -----");
+            System.out.println("-------------------------------------");
+
+            System.out.println("Server is waiting for client request");
+
+            Socket clientsocket = serversocket.accept();
+            
+            System.out.println("Client connected sucesfully");
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception occured : "+e);
+        }
+    }
+}
---
+++ program920.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class program920
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        try
+        {
+            System.out.println("-------------------------------------");
+            System.out.println("----- Marvellous Client Started -----");
+            System.out.println("-------------------------------------");
+
+            Socket socket = new Socket(
+                                        "127.0.0.1",
+                                        9000
+                                        );
+
+            System.out.println("Connection with Server is suucesful");
+        
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception occured : "+e);
+        }
+    }
+}
